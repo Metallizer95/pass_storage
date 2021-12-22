df1b83d4f4078add144444ca62fc1b6fc5df7059
Aleksei Gusakov <alekseigusakov@MacBook-Air-Aleksei.local>
2021-12-22 11:30:10 +0300

    add json tags in passport model

--- internal/usecase/passport/model.go
+++ internal/usecase/passport/model.go
@@ -7,59 +7,59 @@ import (
 type Model struct {
 	XMLName xml.Name `xml:"SectionCertificate"`
 	Text    string   `xml:",chardata"`
-	ID      string   `xml:"id,omitempty"`
-	Header  Header   `xml:"Header"`
-	Towers  Towers   `xml:"Towers"`
+	ID      string   `xml:"id,omitempty" json:"id,omitempty"`
+	Header  Header   `xml:"Header" json:"Header"`
+	Towers  Towers   `xml:"Towers" json:"Towers"`
 }
 
 type Header struct {
 	Text             string `xml:",chardata"`
-	SiteID           string `xml:"siteId"`
-	SectionName      string `xml:"sectionName"`
-	SectionID        string `xml:"sectionId"`
-	EchName          string `xml:"echName"`
-	EchkName         string `xml:"echkName"`
-	LocationId       string `xml:"locationId"`
-	WayAmount        string `xml:"wayAmount"`
-	CurrentWay       string `xml:"currentWay"`
-	CurrentWayID     string `xml:"currentWayID"`
-	CHANGEDATA       string `xml:"CHANGEDATA"`
-	InitialMeter     string `xml:"initialMeter"`
-	InitialKm        string `xml:"initialKM"`
-	InitialPK        string `xml:"initialPK"`
-	InitialM         string `xml:"initialM"`
-	PlotLength       string `xml:"plotLength"`
-	SuspensionAmount string `xml:"suspensionAmount"`
+	SiteID           string `xml:"siteId"           json:"siteId"`
+	SectionName      string `xml:"sectionName"      json:"sectionName"`
+	SectionID        string `xml:"sectionId"        json:"sectionId"`
+	EchName          string `xml:"echName"          json:"echName"`
+	EchkName         string `xml:"echkName"         json:"echkName"`
+	LocationId       string `xml:"locationId"       json:"locationId"`
+	WayAmount        string `xml:"wayAmount"        json:"wayAmount"`
+	CurrentWay       string `xml:"currentWay"       json:"currentWay"`
+	CurrentWayID     string `xml:"currentWayID"     json:"currentWayID"`
+	CHANGEDATA       string `xml:"CHANGEDATA"       json:"CHANGEDATA"`
+	InitialMeter     string `xml:"initialMeter"     json:"initialMeter"`
+	InitialKm        string `xml:"initialKM"        json:"initialKM"`
+	InitialPK        string `xml:"initialPK"        json:"initialPK"`
+	InitialM         string `xml:"initialM"         json:"initialM"`
+	PlotLength       string `xml:"plotLength"       json:"plotLength"`
+	SuspensionAmount string `xml:"suspensionAmount" json:"suspensionAmount"`
 }
 
 type Towers struct {
 	Text   string  `xml:",chardata"`
-	Towers []Tower `xml:"Tower"`
+	Towers []Tower `xml:"Tower" json:"Tower"`
 }
 
 type Tower struct {
 	Text           string `xml:",chardata"`
-	Idtf           string `xml:"idtf"`
-	AssetNum       string `xml:"assetNum"`
-	StopSeq        string `xml:"stopSeq"`
-	Km             string `xml:"km"`
-	Pk             string `xml:"pk"`
-	M              string `xml:"m"`
-	TFTYPE         string `xml:"TF_TYPE"`
-	TURN           string `xml:"TURN"`
-	RADIUS         string `xml:"RADIUS"`
-	Number         string `xml:"number"`
-	Distance       string `xml:"distance"`
-	Zigzag         string `xml:"zigzag"`
-	Height         string `xml:"height"`
-	Offset         string `xml:"offset"`
-	Grounded       string `xml:"grounded"`
-	SPEED          string `xml:"SPEED"`
-	SuspensionType string `xml:"suspensionType"`
-	Catenary       string `xml:"catenary"`
-	WireType       string `xml:"WireType"`
-	CountWire      string `xml:"CountWire"`
-	Longitude      string `xml:"longitude"`
-	Latitude       string `xml:"Latitude"`
-	Gabarit        string `xml:"Gabarit"`
+	Idtf           string `xml:"idtf"           json:"idtf"`
+	AssetNum       string `xml:"assetNum"       json:"assetNum"`
+	StopSeq        string `xml:"stopSeq"        json:"stopSeq"`
+	Km             string `xml:"km"             json:"km"`
+	Pk             string `xml:"pk"             json:"pk"`
+	M              string `xml:"m"              json:"m"`
+	TFTYPE         string `xml:"TF_TYPE"        json:"TF_TYPE"`
+	TURN           string `xml:"TURN"           json:"TURN"`
+	RADIUS         string `xml:"RADIUS"         json:"RADIUS"`
+	Number         string `xml:"number"         json:"number"`
+	Distance       string `xml:"distance"       json:"distance"`
+	Zigzag         string `xml:"zigzag"         json:"zigzag"`
+	Height         string `xml:"height"         json:"height"`
+	Offset         string `xml:"offset"         json:"offset"`
+	Grounded       string `xml:"grounded"       json:"grounded"`
+	SPEED          string `xml:"SPEED"          json:"SPEED"`
+	SuspensionType string `xml:"suspensionType" json:"suspensionType"`
+	Catenary       string `xml:"catenary"       json:"catenary"`
+	WireType       string `xml:"WireType"       json:"WireType"`
+	CountWire      string `xml:"CountWire"      json:"CountWire"`
+	Longitude      string `xml:"longitude"      json:"longitude"`
+	Latitude       string `xml:"Latitude"       json:"Latitude"`
+	Gabarit        string `xml:"Gabarit"        json:"Gabarit"`
 }
